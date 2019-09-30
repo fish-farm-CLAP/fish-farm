d42cb0f6e68ca5318c65a0367968af4439b37358
paul bennett <luapkb@gmail.com>
2019-09-30 12:32:41 -0700

    startGlobal eventArray randomSelector oneF twoF

---
+++ js/randomeEvent.js
@@ -0,0 +1,45 @@
+// global variables
+var food = 0;
+var money = 0
+var eventRun = document.getElementById ('ramdomEventDiv') 
+var eventText = document.createElement ('p');
+eventRun.appendChild(eventText)
+
+
+// aray of randome funtion events 
+var eventOption = []
+// array witll be chosen at ramdom
+//random source generator from array eventOption in constructor
+function choiceGeni(){
+  var eventSelect = Math.floor(Math.random() * eventOption.length );
+  // console.log('raySelectt', raySelect);
+  return eventSelect;
+}
+
+
+// funtion will have a name 
+function tooMuchFood(){
+if ( food <= 3){
+  food-=2;
+ } eventText.textContent = 'the lid fell off your feeder and you have overfed you fish and lost food' 
+
+}
+eventOption.push(tooMuchFood()); 
+
+function winFish();
+if (fish <== 1);
+newFish();
+fish++
+
+
+
+
+// each array will have Text what is happening "text "
+// what the affect is "incriment counter, exicute other funtions 
+
+// funtion to select random Array
+
+// assigne randome array as text
+
+
+incrament
